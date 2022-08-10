369e3d079179dd2fc2016ca3ba03e0ea8654d406
ahmed salem <ahmedsj2007@gmail.com>
2022-08-10 23:56:11 +0300

    ahmed salem
    ahmed salem

---
+++ ahmed salem/Assets/day5.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class day5 : MonoBehaviour
+{
+
+    Rigidbody2D RB;
+
+    float playerInput;
+
+    public float speed;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        playerInput = Input.GetAxis("Horizontal");
+        RB.velocity = new Vector2(playerInput * speed, 0);
+    }
+}
